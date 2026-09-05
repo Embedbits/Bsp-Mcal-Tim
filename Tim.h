4bcb94dcc599cc5398e2c334bf775696ddecc4f0
BSP Release <release@bsp>
2026-09-05 20:04:43 +0000

    Release v1.0.0 for STM32G4

---
+++ Tim.h
@@ -0,0 +1,35 @@
+/**
+ * \author Mr.Nobody
+ * \file Tim.h
+ * \ingroup Tim
+ * \brief Tim module common functionality header file.
+ *
+ * This file contains the common functionality used internally by the module,
+ * and shall provide interface between the module and the application.
+ *
+ */
+
+#ifndef TIM_TIM_H
+#define TIM_TIM_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* ============================= INCLUDES =================================== */
+#include "Tim_Types.h"                      /* Module types definition        */
+/* ============================= TYPEDEFS =================================== */
+
+/* ========================= SYMBOLIC CONSTANTS ============================= */
+
+/* ========================= EXPORTED MACROS ================================ */
+
+/* ========================= EXPORTED VARIABLES ============================= */
+
+/* ======================== EXPORTED FUNCTIONS ============================== */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* TIM_TIM_H */
